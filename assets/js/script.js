903ae99a3f68ab6463a27c8f718be5027b490ee1
網新大麥克 <news@wangxindamaikede-iMac.local>
2020-08-05 10:43:48 +0800

    0805

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+function ajax_gsheet() {
+    $.ajax({
+        url: 'https://spreadsheets.google.com/feeds/cells/1UVnq9a1zVIfygplsbOjOtMX2Bu6aUfet1PwN3MOM7bk/2/public/full?alt=json',
+
+        type: 'GET',
+        dataType: 'json',
+        success: function data_sheet(data) {
+            // 計算 progress width 用的變數
+            let playerVote1 = data.feed.entry[4].content.$t
+            let playerVote2 = data.feed.entry[5].content.$t
+            let playerVote3 = data.feed.entry[6].content.$t
+            let progressTotal = data.feed.entry[7].content.$t
+
+
+            let plProBar1 = playerVote1 / progressTotal * 100
+            let plProBar2 = playerVote2 / progressTotal * 100
+            let plProBar3 = playerVote3 / progressTotal * 100
+
+            $(".num1_bar").css("width", plProBar1.toFixed(2) + "%")
+            $(".num2_bar").css("width", plProBar2.toFixed(2) + "%")
+            $(".num3_bar").css("width", plProBar3.toFixed(2) + "%")
+
+
+            $(".num1_vote span").html(data.feed.entry[4].content.$t)
+            $(".num1_mb_vote span").html(data.feed.entry[4].content.$t)
+
+            $(".num2_vote span").html(data.feed.entry[5].content.$t)
+            $(".num2_mb_vote span").html(data.feed.entry[5].content.$t)
+
+            $(".num3_vote span").html(data.feed.entry[6].content.$t)
+            $(".num3_mb_vote span").html(data.feed.entry[6].content.$t)
+
+        }
+    });
+} ajax_gsheet();
+
+setInterval(ajax_gsheet, 15000)
+
 // 陳其邁資料
 let player1 = {
     // 照片資料
@@ -46,7 +84,7 @@ let player1 = {
             </li>
         </ul>
     `,
-    vote: "未開票"
+    vote: ""
 }
 // 吳益政資料
 let player2 = {
@@ -84,7 +122,7 @@ let player2 = {
             </li>
         </ul>
     `,
-    vote: "未開票"
+    vote: ""
 }
 // 李眉蓁資料
 let player3 = {
@@ -119,7 +157,7 @@ let player3 = {
             </li>
         </ul>
     `,
-    vote: "未開票"
+    vote: ""
 }
 
 // 候選人顯示物件
@@ -1434,9 +1472,6 @@ $(".path2").click(function (e) {
     }
 
 })
-
-
-
 
 
 $(".goToTop").click(function () {
